b5f3ab334478a00f8e233a816381746566d0aec7
levin <levinxo@gmail.com>
2013-07-09 01:25:00 +0800

    js脚本提交

---
+++ cookie.js
@@ -0,0 +1,20 @@
+/**
+ * 获取某个指定的cookie值
+ * It works with jQuery
+ * @param name 需要获取cookie的key值
+ * @return cookieValue 获取到的相应cookie值
+ */
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
