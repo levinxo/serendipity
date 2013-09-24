6d3089d8f28e2a33ae29838d0935d7416c3a4339
levin <levinxo@gmail.com>
2013-09-24 10:25:39 +0800

    读图再保存

--- py_fetch_bing_img_everyday.py
+++ py_fetch_bing_img_everyday.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re, socket, select
+import re, socket, select, time, urlparse
 
 
 def _req(url):
-    r = '''GET / HTTP/1.1\r\nHost: '''+url+'''\r\n\r\n'''
+    url_arr = urlparse.urlparse(url)
+    r = 'GET '+(url_arr[2] if url_arr[2] != '' else '/')+' HTTP/1.1\r\nHost: '+url_arr[1]+'\r\n\r\n'
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((url, 80))
+    s.connect((url_arr[1], 80))
     s.send(r)
     data = ''
     e_count = 0
@@ -30,10 +31,32 @@ def _req(url):
         
         print e_count
         
+    s.close()
     return data
     
 
 if __name__ == '__main__':
-    data = _req('cn.bing.com')
-    print 'total data length: ', len(data)
+    while True:
+        data = _req('http://cn.bing.com')
+        if len(data) > 0:
+            break
+    
+    reg = re.compile("g_img=\{url:'([^']+)'")
+    m = reg.search(data)
+    if m is not None:
+        print m.group(1)
+        while True:
+            img_data = _req(m.group(1))
+            if len(img_data) > 0:
+                break
+        f = open(time.strftime('%Y-%m-%d')+'.'+m.group(1).split('.')[-1], 'wb')
+        img_arr = img_data.split('\r\n\r\n')
+        img_arr.pop(0)
+        img_data = ''.join(img_arr)
+        f.write(img_data)
+        f.close()
+        print 'fetch img success.'
+        
+    else:
+        print 'img not found.'
     
