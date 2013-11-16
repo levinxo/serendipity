fb9bc2344d3774101f8a1836cc4e01595bd287f0
levin <levinxo@gmail.com>
2013-11-16 16:04:00 +0800

    取消数组全局变量，将数组的指针传入函数

--- c_sorting_algo/sort_bubble.c
+++ c_sorting_algo/sort_bubble.c
@@ -4,34 +4,34 @@
 
 #define LEN 20
 #define RAN_MAX 50
-int a[LEN] = {0};
 
-void init(void){
+void init(int *a, int length){
 	int i;
 	srand(time(NULL));
-	for (i=0; i<LEN; i++){
+	for (i=0; i<length; i++){
 		a[i] = rand() % RAN_MAX;
 	}
 }
 
-void sort(void){
+void sort(int *a, int length){
 	int i, j, tmp;
-	for (i=0; i<LEN; i++){
-		for (j=0; j<LEN-1; j++){
-			if (a[j]>a[j+1]){
+	for (i=0; i<length-1; i++){
+		for (j=length-1; j>i; j--){
+			if (a[j]<a[j-1]){
 				tmp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = tmp;
+				a[j] = a[j-1];
+				a[j-1] = tmp;
 			}
 		}
-	}
-	for (i=0; i<LEN; i++){
-		printf("%d\t", a[i]);
 	}
 }
 
 int main(void){
-	init();
-	sort();
+    int a[LEN] = {0}, i;
+	init(a, LEN);
+	sort(a, LEN);
+	for (i=0; i<LEN; i++){
+		printf("%d\t", a[i]);
+	}
 	return 0;
 }
