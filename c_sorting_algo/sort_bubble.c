368f5a51ff19674fc3733a1cf973fe9ba4a8886f
levin <levinxo@gmail.com>
2013-11-16 16:31:37 +0800

    数组名就是一个指针

--- c_sorting_algo/sort_bubble.c
+++ c_sorting_algo/sort_bubble.c
@@ -9,7 +9,7 @@ void init(int *a, int length){
 	int i;
 	srand(time(NULL));
 	for (i=0; i<length; i++){
-		a[i] = rand() % RAN_MAX;
+		*(a+i) = rand() % RAN_MAX;
 	}
 }
 
@@ -17,10 +17,10 @@ void sort(int *a, int length){
 	int i, j, tmp;
 	for (i=0; i<length-1; i++){
 		for (j=length-1; j>i; j--){
-			if (a[j]<a[j-1]){
-				tmp = a[j];
-				a[j] = a[j-1];
-				a[j-1] = tmp;
+			if (*(a+j)<*(a+j-1)){
+				tmp = *(a+j);
+				*(a+j) = *(a+j-1);
+				*(a+j-1) = tmp;
 			}
 		}
 	}
@@ -31,7 +31,7 @@ int main(void){
 	init(a, LEN);
 	sort(a, LEN);
 	for (i=0; i<LEN; i++){
-		printf("%d\t", a[i]);
+		printf("%d\t", *(a+i));
 	}
 	return 0;
 }
